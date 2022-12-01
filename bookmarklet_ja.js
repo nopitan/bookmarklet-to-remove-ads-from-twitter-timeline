117ad9bafc5a510a8250754d62e6c49df3f3560c
Chief Nopitan <86516099+nopitan@users.noreply.github.com>
2022-12-02 07:24:14 +0900

    Add bookmarklet_ja.js

---
+++ bookmarklet_ja.js
@@ -0,0 +1,1 @@
+javascript: ((adWord) => { function removeAds() { Array.prototype.filter.call(document.querySelectorAll("span"), (e) => e.innerText?.includes(adWord)).forEach((e) => { const target = e.closest("[data-testid=cellInnerDiv]"); if (target) target.style.display = "none" }); setTimeout(removeAds, 1000) } setTimeout(removeAds) })("プロモーション")
